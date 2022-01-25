f8f14aaf31ae581adbf2cd6c76dec77b073549a5
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-01-25 22:40:50 +0530

    Added solution - LeetHub

--- Arithmetic Number - GFG/arithmetic-number.java
+++ Arithmetic Number - GFG/arithmetic-number.java
@@ -26,12 +26,21 @@ class GFG{
 
 class Solution{
     static int inSequence(int A, int B, int C){
-      if(A==B)
-return 1;
-if(C!=0){
-int res=(B-A)%C;
-if(res==0 && ( ((B-A)<0 && C<0) || ((B-A)>0 && C>0)))
-return 1;}
-return 0;
+        if(C==0)
+        {
+            if(A==B)return 1;
+            else return 0;
+        }
+     B = B-A;
+    if(B/C < 0)
+    {
+        return 0;
     }
+    if((B)%C==0)
+    {
+        return 1;
+    }
+     return 0;
+     
+}
 }
