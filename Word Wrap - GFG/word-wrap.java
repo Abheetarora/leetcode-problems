6d13ae0f4998fce1666241b39931c7d5deccc996
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-02-02 12:29:48 +0530

    Added solution - LeetHub

--- Word Wrap - GFG/word-wrap.java
+++ Word Wrap - GFG/word-wrap.java
@@ -26,71 +26,29 @@ class GFG
 
 class Solution
 {
-    private int square(int n) {
-        return n * n;
-    }
- 
     public int solveWordWrap (int[] nums, int k)
     {
-        int[][] memo = new int[nums.length][k + 1];
-        for (int i = 0; i < nums.length; i++) {
-            Arrays.fill(memo[i], -1);
+        int dp[][] = new int[nums.length][k+1];
+         for (int i = 0; i < nums.length; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return solveWordWrapUsingMemo(nums, nums.length, k, 0, k, memo);
+        return ans(dp,nums,k,0,k);
         // Code here 
-    }
-    
-    private int solveWordWrap(int[] words, int n, int length, int wordIndex, int remLength, int[][] memo) {
- 
-        //base case for last word
-        if (wordIndex == n - 1) {
-            memo[wordIndex][remLength] = words[wordIndex] < remLength ? 0 : square(remLength);
-            return memo[wordIndex][remLength];
-        }
- 
-        int currWord = words[wordIndex];
-        //if word can fit in the remaining line
-        if (currWord < remLength) {
-            return Math.min(
-                    //if word is kept on same line
-                    solveWordWrapUsingMemo(words, n, length, wordIndex + 1, remLength == length ? remLength - currWord : remLength - currWord - 1, memo),
-                    //if word is kept on next line
-                    square(remLength) + solveWordWrapUsingMemo(words, n, length, wordIndex + 1, length - currWord, memo)
-            );
-        } else {
-            //if word is kept on next line
-            return square(remLength) + solveWordWrapUsingMemo(words, n, length, wordIndex + 1, length - currWord, memo);
-        }
-    }
- 
-    private int solveWordWrapUsingMemo(int[] words, int n, int length, int wordIndex, int remLength, int[][] memo) {
-        if (memo[wordIndex][remLength] != -1) {
-            return memo[wordIndex][remLength];
-        }
- 
-        memo[wordIndex][remLength] = solveWordWrap(words, n, length, wordIndex, remLength, memo);
-        return memo[wordIndex][remLength];
     }
     public int ans (int dp[][],int nums[],int k,int i,int rem )
     {
         if(i == nums.length-1)
         {
-           dp[i][rem] = nums[i] < rem ? 0 : (int)Math.pow(rem,2); 
+           return dp[i][rem] = nums[i] < rem ? 0 : (int)Math.pow(rem,2); 
         }
         int cur = nums[i];
         if(cur < rem)
         {
-            //  int ans1 = 0;//agr same line mei addition krni ho
-            //  int ans2 = 0;//agr doosri line mei jaana ho
-            //  ans1 = check( dp,nums,k,i+1,rem == k ? rem-cur : rem-cur-1 );
-            //  ans2 = (int)Math.pow(rem,2) + check( dp,nums,k,i+1,k-cur );
-            //  return Math.min(ans1,ans2);
-           return Math.min(
-                    //if word is kept on same line
-                    check( dp,nums,k,i+1,rem == k ? rem-cur : rem-cur-1 ),
-                    //if word is kept on next line
-                    (int)Math.pow(rem,2) + check( dp,nums,k,i+1,k-cur )
-            );
+             int ans1 = 0;//agr same line mei addition krni ho
+             int ans2 = 0;//agr doosri line mei jaana ho
+             ans1 = check( dp,nums,k,i+1,rem == k ? rem-cur : rem-cur-1 );
+             ans2 = (int)Math.pow(rem,2) + check( dp,nums,k,i+1,k-cur );
+             return Math.min(ans1,ans2);
         }
         else
         {
