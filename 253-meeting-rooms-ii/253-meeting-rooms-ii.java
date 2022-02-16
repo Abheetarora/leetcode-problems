fed891e20588b1eb1d773d1d2ed03fd5384e3f07
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-02-17 01:10:50 +0530

    Time: 9 ms (59.04%), Space: 45.3 MB (16.54%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,22 +1,47 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        int arr[] = new int[1000001];
-        for(int i=0;i<intervals.length;i++)
-        {
-            fill(arr,intervals[i][0],intervals[i][1]);
-        }
-        int ans = Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++)
-        {
-            ans = Math.max(ans,arr[i]);
-        }
-        return ans;
+        
+    // Check for the base case. If there are no intervals, return 0
+    if (intervals.length == 0) {
+      return 0;
     }
-    public void fill(int arr[],int start,int end)
-    {
-        for(int i=start;i<end;i++)
-        {
-            arr[i]++;
-        }
+
+    // Min heap
+    PriorityQueue<Integer> allocator =
+        new PriorityQueue<Integer>(
+            intervals.length,
+            new Comparator<Integer>() {
+              public int compare(Integer a, Integer b) {
+                return a - b;
+              }
+            });
+
+    // Sort the intervals by start time
+    Arrays.sort(
+        intervals,
+        new Comparator<int[]>() {
+          public int compare(final int[] a, final int[] b) {
+            return a[0] - b[0];
+          }
+        });
+
+    // Add the first meeting
+    allocator.add(intervals[0][1]);
+
+    // Iterate over remaining intervals
+    for (int i = 1; i < intervals.length; i++) {
+
+      // If the room due to free up the earliest is free, assign that room to this meeting.
+      if (intervals[i][0] >= allocator.peek()) {
+        allocator.poll();
+      }
+
+      // If a new room is to be assigned, then also we add to the heap,
+      // If an old room is allocated, then also we have to add to the heap with updated end time.
+      allocator.add(intervals[i][1]);
     }
+
+    // The size of the heap tells us the minimum rooms required for all the meetings.
+    return allocator.size();
+  }
 }
