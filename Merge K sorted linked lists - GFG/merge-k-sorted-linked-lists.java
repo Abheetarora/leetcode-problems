7b395e865e006c03a2bd58588b1c3aacc6d29d5b
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-02-17 16:15:51 +0530

    Added solution - LeetHub

---
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.java
@@ -0,0 +1,111 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// a is an array of Nodes of the heads of linked lists
+// and N is size of array a
+
+
+class Solution
+{
+    //Function to merge K sorted linked list.
+      Node mergeKList(Node[]arr,int K)
+    {
+        int k=arr.length;
+        if(k==0) return null;
+        Node head=null,temp=null;
+        PriorityQueue<Integer> heap=new PriorityQueue<Integer>();
+        for(int i=0;i<K;i++){
+            temp=arr[i];
+            while(temp!=null){
+                heap.add(temp.data);
+                temp=temp.next;
+            }
+        }
+        int i=0;
+        while(i<heap.size()){
+            if(head==null){
+                head=new Node(heap.poll());
+                temp=head;
+            }else{
+               temp.next=new Node(heap.poll());
+             temp=temp.next; 
+            }
+             
+        }
+        return head;
+    }
+}
