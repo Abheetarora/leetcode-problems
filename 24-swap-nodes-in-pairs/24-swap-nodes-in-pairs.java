1739a457f3d491045130b320c1b3af032bfacd5a
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-02-20 08:00:18 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (47.56%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -9,9 +9,11 @@ class Solution {
         while(curr.next!=null && curr.next.next!=null){
             ListNode f_node=curr.next;
             ListNode s_node=curr.next.next;
+            
             f_node.next=s_node.next; // 1->3
             curr.next=s_node; //0->2
             s_node.next=f_node; //2->1
+            
             curr=curr.next.next;  // Similarly doing for otther nodes
         }
         return temp.next;  //2->1->4>3
