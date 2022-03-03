a0db9a8d3bf2888c89fb9e78b1269c13ec2288ea
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-03-04 01:07:47 +0530

    Added solution - LeetHub

---
+++ Maximum sum of Non-adjacent nodes - GFG/maximum-sum-of-nonadjacent-nodes.java
@@ -0,0 +1,155 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node left, right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class MaxSum
+{
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution ob = new Solution();
+			    int ans = ob.getMaxSum(root);
+			    System.out.println(ans);
+                t--;
+	        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*class Node
+{
+    int data;
+    Node left, right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}*/
+
+class Solution
+{
+    //Function to return the maximum sum of non-adjacent nodes.
+    static int getMaxSum(Node root) {
+        // Here HashMap is to reduce the time complexity
+        HashMap<Node, Integer> hm = new HashMap<>();
+        return solve(root, hm);
+    }
+
+    static int solve(Node root, HashMap<Node, Integer> hm) {
+        if (root == null)
+            return 0;
+
+        if (hm.containsKey(root))
+            return hm.get(root);
+
+        int include = 0;
+        int exclude = 0;
+
+        include += root.data;
+
+        if (root.left != null) {
+            include += solve(root.left.left, hm);
+            include += solve(root.left.right, hm);
+        }
+        if (root.right != null) {
+            include += solve(root.right.left, hm);
+            include += solve(root.right.right, hm);
+        }
+
+        exclude += solve(root.left, hm);
+        exclude += solve(root.right, hm);
+
+        hm.put(root, Math.max(include, exclude));
+        return Math.max(include, exclude);
+
+    }
+}
