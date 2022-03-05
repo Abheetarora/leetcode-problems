49297cfb83a95e81b8e28ea49061f7fddc904c8c
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-03-06 01:03:20 +0530

    Time: 185 ms (25.00%), Space: 130.2 MB (25.00%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,26 +1,49 @@
 class Solution {
+    List<Integer> [] graph;
+    boolean [] visited;
+    List<Integer> [] result;
+    
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-    List<List<Integer>> ans = new ArrayList();
-    List<List<Integer>> directChild = new ArrayList();
-    for (int i = 0; i < n; i++) {
-        ans.add(new ArrayList());
-        directChild.add(new ArrayList());
-    }
-    for (int[] e: edges) 
-        directChild.get(e[0]).add(e[1]);
-    for (int i = 0; i < n; i++) {
-        Set<Integer> visited = new HashSet();
-        dfs(i, i, visited, ans, directChild);
-    }
-    return ans;
-}
-private void dfs(int x, int curr, Set<Integer> visited, List<List<Integer>> ans, List<List<Integer>> directChild) {
-    visited.add(curr);
-    for (int ch: directChild.get(curr)) {
-        if(!visited.contains(ch)) {
-            ans.get(ch).add(x);
-            dfs(x, ch, visited, ans, directChild);
+        graph = new List[n];
+        visited = new boolean[n];
+        result = new List[n];
+        
+        for (int i = 0; i < n; ++i)
+            graph[i] = new ArrayList<>();
+        
+        for (int [] edge : edges){
+            graph[edge[1]].add(edge[0]);
+            visited[edge[0]] = true;
         }
+        
+        for (int i = 0; i < n; ++i){
+            if (visited[i]) continue;
+            
+            result[i] = getList(i);
+        }
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for (List<Integer> list : result){
+            ans.add(list);
+            Collections.sort(list);
+        }
+        
+        return ans;
     }
-}
+    
+    private List<Integer> getList(int node){
+        if (result[node] != null)
+            return result[node];
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int child : graph[node]){
+            set.add(child);
+            set.addAll(getList(child));
+        }
+        
+        result[node] = new ArrayList<>(set);
+        return result[node];
+    }
 }
