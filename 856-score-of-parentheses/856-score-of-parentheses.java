52ae42db6603a3c6f858c31a92764eb56674cdad
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-03-17 16:38:48 +0530

    Time: 1 ms (68.54%), Space: 42.5 MB (7.07%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int scoreOfParentheses(String str) {
-       Stack<Integer> s=new Stack<>();
-        s.push(0);
-        for(char ch:str.toCharArray()){
-            if(ch=='('){
-                s.push(0);
-            }else{
-                int v=s.pop();
-                int w=s.pop();
-                s.push(w+Math.max(2*v,1));
-            }
-        }
-        return s.pop();
-    }
+	public int scoreOfParentheses(String s) {
+		Stack<Integer> stack = new Stack<Integer>();
+		int score = 0;
+
+		for (char c : s.toCharArray()) {
+			if (c == '(') {
+				stack.push(score);
+				score = 0;
+			} else {
+				score = stack.pop() + Math.max(score * 2, 1);
+			}
+		}
+		return score;
+	}
 }
