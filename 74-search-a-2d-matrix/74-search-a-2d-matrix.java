53512648d7bf0f012ea30028a10319e31ab95547
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-03-30 17:00:02 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (45.83%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,25 +1,20 @@
-class Solution {
+public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        if (m == 0)
-            return false;
-        int n = matrix[0].length;
-
-        // binary search
-        int left = 0, right = m * n - 1;
-        int pivotIdx, pivotElement;
+        if (matrix == null || matrix.length == 0|| matrix[0] == null) return false;
+        int row = matrix.length, col = matrix[0].length;
+        int left = 0, right = row*col -1;
         while (left <= right) {
-            pivotIdx = (left + right) / 2;
-            pivotElement = matrix[pivotIdx / n][pivotIdx % n];
-            if (target == pivotElement)
-                return true;
-            else {
-                if (target < pivotElement)
-                    right = pivotIdx - 1;
-                else
-                    left = pivotIdx + 1;
+            int mid = left + (right - left)/2;
+            int r = mid/col;
+            int c = mid%col;
+            if (matrix[r][c] == target) return true;
+            else if (matrix[r][c] < target) {
+                left = mid + 1;
+            } else {
+                right = mid -1;
             }
-        }
+            
+        }//while
         return false;
     }
 }
