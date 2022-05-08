2f4a3dc1ec4cac01ef8d113c3da75b0aa299ca72
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-05-08 17:29:09 +0530

    Time: 257 ms (34.19%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,8 +1,1 @@
-SELECT
-    (SELECT DISTINCT
-            Salary
-        FROM
-            Employee
-        ORDER BY Salary DESC
-        LIMIT 1 OFFSET 1) AS SecondHighestSalary
-;
+ select max(salary) as  SecondHighestSalary  from employee where salary<(select max(salary) from employee);
