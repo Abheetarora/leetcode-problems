47ed0057490ce0088297c81c6560db967a4792aa
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-05-11 17:53:12 +0530

    Time: 285 ms (91.74%), Space: 0B (100.00%) - LeetHub

---
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+     SELECT distinct salary
+     FROM (
+         SELECT salary, DENSE_RANK() OVER(ORDER BY salary DESC) as rn
+         FROM Employee
+     ) as sub_select
+     WHERE rn = N
+  );
+END
