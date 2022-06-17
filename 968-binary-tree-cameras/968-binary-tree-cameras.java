025aa350a32201f5586a1849191985645b1a7213
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-06-17 13:57:33 +0530

    Time: 2 ms (20.45%), Space: 42.3 MB (79.87%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    Set<TreeNode>st;
+    public int minCameraCover(TreeNode root) {
+        st = new HashSet<>();
+        st.add(null);
+        dfs(root,null);
+        return ans;
+    }
+    public void dfs(TreeNode node,TreeNode parent)
+    {
+        if(node != null)
+        {
+            dfs(node.left,node);
+            dfs(node.right,node);
+            if(parent == null && !st.contains(node) || !st.contains(node.left) || !st.contains(node.right))
+            {
+                ans++;
+                st.add(node);
+                st.add(parent);
+                st.add(node.left);
+                st.add(node.right);
+            }
+        }
+    }
+}
