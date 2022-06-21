6a1e4655c880a1afec273799b897c273a9d9196d
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-06-22 00:00:16 +0530

    Time: 766 ms (5.00%), Space: 120.3 MB (5.17%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,23 +1,39 @@
 class Solution {
-    public int furthestBuilding(int[] arr, int b, int l) {
-        PriorityQueue<Integer>pq = new PriorityQueue<>();
-        for(int i =0;i<arr.length-1;i++)
-        {
-            if(arr[i+1]-arr[i]<=0)
-            {
-                continue;
-            }
-            pq.add(arr[i+1]-arr[i]);
-            if(pq.size()<=l)
-            {
-                continue;
-            }
-            b = b - pq.poll();
-            if(b<0)
-            {
-                return i;
+    
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        int lo = 0;
+        int hi = heights.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo + 1) / 2;
+            if (isReachable(mid, heights, bricks, ladders)) {
+                lo = mid;
+            } else {
+                hi = mid - 1;
             }
         }
-        return arr.length-1;
+        return hi; 
+    }
+    
+    private boolean isReachable(int buildingIndex, int[] heights, int bricks, int ladders) {
+        List<Integer> climbs = new ArrayList<>();
+        for (int i = 0; i < buildingIndex; i++) {
+            int h1 = heights[i];
+            int h2 = heights[i + 1];
+            if (h2 <= h1) {
+                continue;
+            }
+            climbs.add(h2 - h1);
+        }
+        Collections.sort(climbs);
+        for (int climb : climbs) {
+            if (climb <= bricks) {
+                bricks -= climb;
+            } else if (ladders >= 1) {
+                ladders -= 1;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
