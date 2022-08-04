376fbb645ccf176d311530397fa9a6c2ee17f260
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-08-05 01:24:20 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (69.44%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -8,6 +8,6 @@ class Solution {
         if (m % 2 == 0 && n % 2 == 1) return 0;
         if (m % 2 == 1 && n % 2 == 1) return 1;
         if (m % 2 == 1 && n % 2 == 0) return 2;
-        return -1;
+        return 0;
     }
 }
