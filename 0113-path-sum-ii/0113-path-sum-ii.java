af005c3aa2b9f844be185e2e548b28d5b7478df6
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2022-10-15 00:42:20 +0530

    Time: 1 ms (100.00%), Space: 45.1 MB (24.28%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public void recurseTree(TreeNode node, int remainingSum, List<Integer> pathNodes, List<List<Integer>> pathsList) {
+        
+        if (node == null) {
+            return;
+        }
+        
+        pathNodes.add(node.val);
+        
+        if (remainingSum == node.val && node.left == null && node.right == null) {
+            pathsList.add(new ArrayList<>(pathNodes));
+        } else {
+            
+            recurseTree(node.left, remainingSum - node.val, pathNodes, pathsList);
+            recurseTree(node.right, remainingSum - node.val, pathNodes, pathsList);
+        }
+        
+        pathNodes.remove(pathNodes.size() - 1);
+    }
+    
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> pathsList = new ArrayList<List<Integer>>();
+        List<Integer> pathNodes = new ArrayList<Integer>();
+        recurseTree(root, sum, pathNodes, pathsList);
+        return pathsList;        
+    }
+}
